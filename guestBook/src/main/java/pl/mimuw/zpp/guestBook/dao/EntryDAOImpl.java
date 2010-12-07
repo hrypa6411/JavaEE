2a30e3831cea38091da3b0364c43d1804abf17e3
Jarek Hryciuk <jarek@maxim.maxim-net>
2010-12-07 22:40:44 +0100

    Cos

--- guestBook/src/main/java/pl/mimuw/zpp/guestBook/dao/EntryDAOImpl.java
+++ guestBook/src/main/java/pl/mimuw/zpp/guestBook/dao/EntryDAOImpl.java
@@ -3,42 +3,32 @@ package pl.mimuw.zpp.guestBook.dao;
 import java.util.Date;
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.transaction.annotation.Transactional;
 
 import pl.mimuw.zpp.guestBook.domain.Entry;
 
-public class EntryDAOImpl implements EntryDAO {
-
-	private SessionFactory sessionFactory;
-
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+public class EntryDAOImpl extends HibernateDaoSupport implements EntryDAO {
 	
+	private Log log = LogFactory.getLog(EntryDAOImpl.class);
 	
 	public EntryDAOImpl() {
-		System.out.println("Utworzyłem obiekt EntryDAOImpl");
+		log.info("Utworzyłem obiekt EntryDAOImpl");
 	}
 
 	@Override
 	@Transactional
 	public void saveEntry(Entry e) {
-		Session session = this.sessionFactory.openSession();
 		e.setDate(new Date());
-		session.beginTransaction();
-		session.save(e);
-		session.getTransaction().commit();		
+		getHibernateTemplate().save(e);
 	}
 	
 	@Override
 	@Transactional(readOnly = true)
 	public List<Entry> getEntries() {
-		Session session = this.sessionFactory.openSession();
-		session.beginTransaction();
-		List<Entry> entries = (List<Entry>)session.createQuery("from Entry").list();
-		session.getTransaction().commit();
+		List<Entry> entries = (List<Entry>) getHibernateTemplate().find("from Entry");
 		return entries;
 	}
 
